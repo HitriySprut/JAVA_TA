71cea004ec8afbbc1bc7d43537e5f7c857eb34a1
Ksenia <hitriysprut@gmail.com>
2018-05-05 20:30:12 +0300

    renamed

---
+++ sandbox/src/main/java/ksenia/ta_java/sandbox/Program.java
@@ -0,0 +1,8 @@
+package ksenia.ta_java.sandbox;
+
+public class Program {
+
+  public static void main(String[] args) {
+    System.out.print("hehehehe");
+  }
+}
--- sandbox/src/main/java/ksenia/ta_java/sandbox/program.java
+++
@@ -1,8 +0,0 @@
-package ksenia.ta_java.sandbox;
-
-public class program {
-
-    public static void main(String[] args) {
-        System.out.print("hehehehe");
-    }
-}
