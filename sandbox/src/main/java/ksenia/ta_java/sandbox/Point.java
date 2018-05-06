0aa60aa60d7119552ceed91c7045860eb45f8d14
Ksenia <hitriysprut@gmail.com>
2018-05-06 23:03:51 +0300

    homework 2 - calculate distance

---
+++ sandbox/src/main/java/ksenia/ta_java/sandbox/Point.java
@@ -0,0 +1,43 @@
+package ksenia.ta_java.sandbox;
+
+
+/**
+ * Created by admin on 06.05.2018.
+ */
+public class Point {
+  private int x, y;
+
+  public static double distance(Point p1, Point p2) {
+    double res = Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+    return res;
+  }
+
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+
+  }
+
+  public void setY(int x) {
+    this.y = y;
+
+  }
+
+  public int getX() {
+    return x;
+
+  }
+
+  public int getY() {
+    return y;
+
+  }
+
+  public String toString(){
+    return "("+x+":"+y+")";
+  }
+}
--- sandbox/src/main/java/ksenia/ta_java/sandbox/Program.java
+++ sandbox/src/main/java/ksenia/ta_java/sandbox/Program.java
@@ -3,6 +3,10 @@ package ksenia.ta_java.sandbox;
 public class Program {
 
   public static void main(String[] args) {
-    System.out.print("hehehehe");
+
+    Point point1 = new Point(5,4);
+    Point point2 = new Point(-4,125);
+    System.out.println("Расстояние между точками "+point1+" и "+point2+" равно: "+Point.distance(point1,point2));
+
   }
 }
