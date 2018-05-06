de261f01aa74a207f3495ee058a8e6aac5fbf34d
Ksenia <hitriysprut@gmail.com>
2018-05-07 00:50:19 +0300

    add dependencies testNG

--- sandbox/src/main/java/ksenia/ta_java/sandbox/Program.java
+++ sandbox/src/main/java/ksenia/ta_java/sandbox/Program.java
@@ -4,9 +4,9 @@ public class Program {
 
   public static void main(String[] args) {
 
-    Point point1 = new Point(5,4);
-    Point point2 = new Point(-4,125);
-    System.out.println("Расстояние между точками "+point1+" и "+point2+" равно: "+Point.distance(point1,point2));
+    Point point1 = new Point(5, 4);
+    Point point2 = new Point(-4, 125);
+    System.out.println("Расстояние между точками " + point1 + " и " + point2 + " равно: " + Point.distance(point1, point2));
 
   }
 }
