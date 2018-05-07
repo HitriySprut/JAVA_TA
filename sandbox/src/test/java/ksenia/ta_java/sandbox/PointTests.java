988b552b92063b5804765fe5a77803f4cba47050
ksenia <hitriysprut@gmail.com>
2018-05-07 15:14:37 +0300

    fixed 3rd task: non-static method used

--- sandbox/src/test/java/ksenia/ta_java/sandbox/PointTests.java
+++ sandbox/src/test/java/ksenia/ta_java/sandbox/PointTests.java
@@ -11,7 +11,7 @@ public class PointTests {
   public void testDistance() {
     Point p1 = new Point(0, 0);
     Point p2 = new Point(3, 4);
-    double res = Point.distance(p1, p2);
+    double res = p1.distance(p2);
     Assert.assertEquals(res, 5.00);
   }
 
@@ -19,7 +19,7 @@ public class PointTests {
   public void testZeroDistance() {
     Point p1 = new Point(0, 0);
     Point p2 = new Point(0, 0);
-    double res = Point.distance(p1, p2);
+    double res = p1.distance(p2);
     Assert.assertEquals(res, 0.0);
   }
 
@@ -27,7 +27,7 @@ public class PointTests {
   public void testMaxDistance() {
     Point p1 = new Point(-2147483648, -2147483648);
     Point p2 = new Point(2147483647, 2147483647);
-    double res = Math.ceil(Point.distance(p1, p2));
+    double res = Math.ceil(p1.distance(p2));
     Assert.assertEquals(res, 6.074000999E9);
   }
 
