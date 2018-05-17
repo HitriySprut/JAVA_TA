6f7c28db8fc2651bec9c49ee91c40b7ee14a9198
ksenia <hitriysprut@gmail.com>
2018-05-17 17:53:13 +0300

    Contact creation test + ContactData class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.*;
 
 public class ContactCreationTests {
     FirefoxDriver wd;
-    
+
     @BeforeMethod
     public void setUp() throws Exception {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
@@ -22,45 +22,47 @@ public class ContactCreationTests {
     }
 
     private void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
+        fillTextBox(textBoxName.user, username);
+        fillTextBox(textBoxName.pass, password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
-    public void ContactCreationTests() {
+    public void testContactCreation() {
 
         initContactCreation();
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Krokodil");
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Gena");
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("777Gena777");
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys("BlueHelicopter");
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("12345");
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("+712345");
-        wd.findElement(By.name("work")).click();
-        wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys("12345");
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("gena_s_avtogenom@fairy.ft");
-        wd.findElement(By.name("submit")).click();
+        fillContactForm(new ContactData("Gena", "Krokodil", "gena_s_avtogenom@fairy.ft"));
+        submitContactCreation();
         returnToHomepage();
+    }
+
+    private void fillTextBox(textBoxName textBoxName, String value) {
+
+        String boxName = textBoxName.name();
+        WebElement element = wd.findElement(By.name(boxName));
+        element.click();
+        element.clear();
+        element.sendKeys(value);
+    }
+    enum textBoxName{
+        user,
+        pass,
+        firstname,
+        lastname,
+        email,
+        nickname,
+        company,
+        mobile
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        fillTextBox(textBoxName.firstname, contactData.getFirstname());
+        fillTextBox(textBoxName.lastname, contactData.getLastname());
+        fillTextBox(textBoxName.email, contactData.getEmail());
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.name("submit")).click();
     }
 
     private void returnToHomepage() {
@@ -75,7 +77,7 @@ public class ContactCreationTests {
     public void tearDown() {
         wd.quit();
     }
-    
+
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
             wd.switchTo().alert();
@@ -84,4 +86,7 @@ public class ContactCreationTests {
             return false;
         }
     }
+
+
+
 }
