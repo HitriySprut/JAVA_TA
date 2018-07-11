c9a2386ab0bd2be6ffd57b482d3f49662a71b4f0
ksenia <hitriysprut@gmail.com>
2018-07-11 17:33:01 +0300

    gradle properties

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -21,7 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
